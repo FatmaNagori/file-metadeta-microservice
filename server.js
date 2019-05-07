8de6d025f8a081fb29247c474e9b0203ede42c35
FatmaNagori <49934458+FatmaNagori@users.noreply.github.com>
2019-05-07 13:34:48 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var express = require('express');
+var cors = require('cors');
+
+// require and use "multer"...
+var multer =require('multer');
+var upload=multer({dest:"upload/"});
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+app.get('/', function (req, res) {
+     res.sendFile(process.cwd() + '/views/index.html');
+  });
+
+app.post('/api/fileanalyse',upload.single('upfile'), function(req, res){
+  var name=req.file.originalname;
+  var type=req.file.mimetype;
+  var size=req.file.size;
+  res.json({name:name,type:type,size:size})
+});
+
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Node.js listening ...');
+});
